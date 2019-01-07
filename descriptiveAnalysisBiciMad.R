5aa5b3d106a68bae698642d48dfb1ce2aa87e85f
Luis Arsenio Méndez Vázquez <lmvazquez@alumni.afi.es>
2019-01-07 20:10:54 +0100

    .

--- descriptiveAnalysisBiciMad.R
+++ descriptiveAnalysisBiciMad.R
@@ -4,20 +4,23 @@ library(dplyr)
 library(lubridate)
 library(stringr)
 
-date <- as.Date(yourdata$Date_of_order, format = "%Y/%m/%d")
-yourdata$WeekDay <- weekdays(date)
 
 stationsData <- fread("Data/3_months_data.csv")
-stations <- stationsData %>% filter(activate == 1, no_available == 0) %>% 
-  select(dock_bikes, free_bases, id, name, no_available, total_bases, fechahora)
-
 dates <- c()
-for (i in 1:length(stations$fechahora)){
-  dates <- c(dates, unlist(str_split(string = stations$fechahora[i], pattern = "T"))[1])
+for (i in 1:length(stationsData$fechahora)){
+  dates <- c(dates, unlist(str_split(string = stationsData$fechahora[i], pattern = "T"))[1])
 }
 
 datesWithFormat <- as_date(dates)
 weekDaysData <- weekdays(datesWithFormat)
-stations$weekDays <- weekDaysData
+monthsData <- month(datesWithFormat)
 
-stations %>% group_by(name) %>% summarise(Rate =  mean(free_bases/total_bases)) %>% arrange(name)
+stationsData$monthsData <- monthsData
+stationsData$weekDay <- weekDaysData
+stationsData$date <- datesWithFormat
+
+stations <- stationsData %>% filter(activate == 1, no_available == 0) %>% 
+  select(dock_bikes, free_bases, id, name, no_available, total_bases, fechahora, weekDay,date, reservations_count, monthsData)
+
+r <- stations %>% group_by(monthsData, weekDay) %>% summarise(Rate =  mean(free_bases/total_bases)) %>% arrange(desc(monthsData))
+stations %>% group_by(monthsData) %>% summarise(Rate =  sum(reservations_count)) %>% arrange(desc(Rate))
