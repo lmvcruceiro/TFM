8eb628ffd3d92c11eff8aa09f07c4fbfb10826aa
Luis Arsenio Méndez Vázquez <lmvazquez@alumni.afi.es>
2019-01-07 20:10:35 +0100

    .

--- leafletBiciMad.R
+++ leafletBiciMad.R
@@ -11,7 +11,7 @@ segovia45 <- c(40.4136841, -3.717536300000006)
 
 
 ##- Cluster 2 --##
-jesusYMaria <- c(40.3882087, -3.7614158999999745)
+jesusYMaria <- c(40.4112154, -3.703640500000006)
 santaMariaDeLaCabeza <- c(40.3957595, -3.7041702000000214)
 palosDeLaFrontera <- c(40.4039957, -3.699952899999971)
 plazaDeLavapies <- c(40.4088039, -3.7012909000000036)
@@ -33,7 +33,7 @@ puertaDeToledo <- c(40.4069778, -3.7114971999999398)
 entradaMatadero <- c(40.3928787, -3.69932689999996)
 paseoDeLaFlorida <- c(40.4226321, -3.72275680000007)
 
-m <- leaflet(data=mydf)%>%
+m <- leaflet()%>%
   addTiles()%>%
   setView(lng = -3.68, lat = 40.43, zoom = 12.5) %>%
   addPolygons(lat = c(
@@ -60,4 +60,4 @@ m <- leaflet(data=mydf)%>%
     paseoDeLaFlorida[2], plazaDeLaCebada[2], embajadores[2],  entradaMatadero[2], metroPiramidesMadrid[2], puertaDeToledo[2]
   ),weight = 2, fillColor = T, color = "violet"
   )
-m
+m 
